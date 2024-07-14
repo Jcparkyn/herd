436e2d5707336cbe8406d95ca73e545786fc93d2
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2024-07-14 21:56:14 +1000

    Nicer debug printing

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,4 @@
-
+use std::fmt::Debug;
 // pub struct Function {
 //   params: Vec<String>,
 //   body: Expr, // or something else?
@@ -13,14 +13,17 @@
 //   Assignment(Vec<ValueIndex>, Expr)
 // }
 
-#[derive(PartialEq, Debug)]
+use std::fmt::Error;
+use std::fmt::Formatter;
+
+#[derive(PartialEq)]
 pub enum Expr {
     Number(f64),
     Bool(bool),
     Op(Box<Expr>, Opcode, Box<Expr>),
 }
 
-#[derive(PartialEq, Eq, Hash, Debug)]
+#[derive(PartialEq, Eq, Hash)]
 pub enum Opcode {
     Mul,
     Div,
@@ -30,4 +33,29 @@ pub enum Opcode {
     Lt,
     Eq,
     Neq,
+}
+
+impl Debug for Expr {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        match self {
+            Expr::Number(n) => n.fmt(f),
+            Expr::Bool(b) => b.fmt(f),
+            Expr::Op(l, op, r) => write!(f, "({:?} {:?} {:?})", l, op, r),
+        }
+    }
+}
+
+impl Debug for Opcode {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error>  {
+        f.write_str(match self {
+            Opcode::Mul => "*",
+            Opcode::Div => "/",
+            Opcode::Add => "+",
+            Opcode::Sub => "-",
+            Opcode::Gt => ">",
+            Opcode::Lt => "<",
+            Opcode::Eq => "==",
+            Opcode::Neq => "!=",
+        })
+    }
 }
