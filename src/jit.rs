8d6aaadcee5a4ccca1f8188f516afa85a18b17a2
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2024-09-15 12:44:40 +1000

    Variable builder part 1

--- src/jit.rs
+++ src/jit.rs
@@ -1,11 +1,14 @@
-// use crate::frontend::*;
+#![allow(dead_code)]
+
 use cranelift::prelude::*;
 use cranelift_jit::{JITBuilder, JITModule};
 use cranelift_module::{DataDescription, Linkage, Module};
 use std::collections::HashMap;
 use std::slice;
 
-use crate::ast::LambdaExpr;
+use crate::ast::{Expr, LambdaExpr, MatchPattern, SpannedExpr};
+
+type FuncExpr = LambdaExpr;
 
 /// The basic JIT class.
 pub struct JIT {
@@ -46,5 +49,166 @@ impl JIT {
             data_description: DataDescription::new(),
             module,
         }
+    }
+
+    pub fn compile_func(&mut self, func: &FuncExpr) -> Result<*const u8, String> {
+        // Then, translate the AST nodes into Cranelift IR.
+        self.translate_func(func)?;
+
+        // Next, declare the function to jit. Functions must be declared
+        // before they can be called, or defined.
+        //
+        // TODO: This may be an area where the API should be streamlined; should
+        // we have a version of `declare_function` that automatically declares
+        // the function?
+        let id = self
+            .module
+            .declare_function(
+                &func.name.clone().unwrap(),
+                Linkage::Export,
+                &self.ctx.func.signature,
+            )
+            .map_err(|e| e.to_string())?;
+
+        // Define the function to jit. This finishes compilation, although
+        // there may be outstanding relocations to perform. Currently, jit
+        // cannot finish relocations until all functions to be called are
+        // defined. For this toy demo for now, we'll just finalize the
+        // function below.
+        self.module
+            .define_function(id, &mut self.ctx)
+            .map_err(|e| e.to_string())?;
+
+        // Now that compilation is finished, we can clear out the context state.
+        self.module.clear_context(&mut self.ctx);
+
+        // Finalize the functions which we just defined, which resolves any
+        // outstanding relocations (patching in addresses, now that they're
+        // available).
+        self.module.finalize_definitions().unwrap();
+
+        // We can now retrieve a pointer to the machine code.
+        let code = self.module.get_finalized_function(id);
+
+        Ok(code)
+    }
+
+    fn translate_func(&mut self, func: &FuncExpr) -> Result<(), String> {
+        // Our toy language currently only supports I64 values, though Cranelift
+        // supports other types.
+        let int = self.module.target_config().pointer_type();
+
+        for _p in &*func.params {
+            self.ctx.func.signature.params.push(AbiParam::new(int));
+        }
+
+        // Our toy language currently only supports one return value, though
+        // Cranelift is designed to support more.
+        self.ctx.func.signature.returns.push(AbiParam::new(int));
+
+        // Create the builder to build a function.
+        let mut builder = FunctionBuilder::new(&mut self.ctx.func, &mut self.builder_context);
+
+        // Create the entry block, to start emitting code in.
+        let entry_block = builder.create_block();
+
+        // Since this is the entry block, add block parameters corresponding to
+        // the function's parameters.
+        //
+        // TODO: Streamline the API here.
+        builder.append_block_params_for_function_params(entry_block);
+
+        // Tell the builder to emit code in this block.
+        builder.switch_to_block(entry_block);
+
+        // And, tell the builder that this block will have no further
+        // predecessors. Since it's the entry block, it won't have any
+        // predecessors.
+        builder.seal_block(entry_block);
+
+        // The toy language allows variables to be declared implicitly.
+        // Walk the AST and declare all implicitly-declared variables.
+        let mut variable_builder = VariableBuilder {
+            int,
+            builder: &mut builder,
+            variables: HashMap::new(),
+            index: 0,
+        };
+        variable_builder.declare_variables_in_func(func, entry_block);
+        let variables = variable_builder.variables;
+
+        // // Now translate the statements of the function body.
+        // let mut trans = FunctionTranslator {
+        //     int,
+        //     builder,
+        //     variables,
+        //     module: &mut self.module,
+        // };
+        // for expr in stmts {
+        //     trans.translate_expr(expr);
+        // }
+
+        // // Emit the return instruction.
+        // trans.builder.ins().return_(&[return_value]);
+
+        // Tell the builder we're done with this function.
+        // trans.builder.finalize();
+        Ok(())
+    }
+}
+
+struct VariableBuilder<'a> {
+    int: types::Type,
+    builder: &'a mut FunctionBuilder<'a>,
+    variables: HashMap<String, Variable>,
+    index: usize,
+}
+
+impl VariableBuilder<'_> {
+    fn declare_variables_in_func(&mut self, func: &FuncExpr, entry_block: Block) {
+        for (i, pattern) in func.params.iter().enumerate() {
+            let name = match pattern.value {
+                MatchPattern::Declaration(ref var, _) => &var.name,
+                _ => todo!("Pattern matching in functions isn't supported"),
+            };
+            let val = self.builder.block_params(entry_block)[i];
+            let var = self.declare_variable(name);
+            self.builder.def_var(var, val);
+        }
+        // let zero = builder.ins().iconst(int, 0);
+        // let return_variable = declare_variable(int, builder, &mut variables, &mut index, the_return);
+        // builder.def_var(return_variable, zero);
+        // for expr in stmts {}
+        self.declare_variables_in_expr(&func.body);
+    }
+
+    fn declare_variables_in_expr(&mut self, expr: &SpannedExpr) {
+        match &expr.value {
+            // Expr::Variable(ref name) => {
+            //     declare_variable(int, builder, variables, index, name);
+            // }
+            Expr::If {
+                condition,
+                then_branch,
+                else_branch,
+            } => {
+                self.declare_variables_in_expr(condition);
+                self.declare_variables_in_expr(then_branch);
+                if let Some(ref else_branch) = else_branch {
+                    self.declare_variables_in_expr(else_branch);
+                }
+            }
+            _ => (),
+        }
+    }
+
+    fn declare_variable(&mut self, name: &str) -> Variable {
+        let var = Variable::new(self.index);
+        if !self.variables.contains_key(name) {
+            self.variables.insert(name.into(), var);
+            self.builder.declare_var(var, self.int);
+            self.index += 1;
+        }
+        var
     }
 }
