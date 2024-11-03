5dab8082ef419c93f23ae019729c62de81a0f186
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2024-11-03 18:43:45 +1100

    Move prelude functions to stdlib

--- src/stdlib/mod.rs
+++ src/stdlib/mod.rs
@@ -1,6 +1,11 @@
 pub fn load_stdlib_module(name: &str) -> &'static str {
     match name {
+        "@basics" => include_str!("basics.bovine"),
+        "@bitwise" => include_str!("bitwise.bovine"),
+        "@dict" => include_str!("dict.bovine"),
+        "@io" => include_str!("io.bovine"),
         "@list" => include_str!("list.bovine"),
+        "@math" => include_str!("math.bovine"),
         _ => panic!("No such stdlib module: {}", name),
     }
 }
